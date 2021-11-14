f81260b02c9eb7f3acb9011c0b4c64713fb777a7
Adlan Kha <amartin0950@gmail.com>
2021-11-14 19:28:59 +0100

    added package controller with classes

---
+++ DevPub/src/main/java/ru/skillbox/devpub/controller/ApiAuthController.java
@@ -0,0 +1,8 @@
+package ru.skillbox.devpub.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ApiAuthController {
+}
---
+++ DevPub/src/main/java/ru/skillbox/devpub/controller/DefaultController.java
@@ -0,0 +1,21 @@
+package ru.skillbox.devpub.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// для обычных запросов не через API (главная страница - /, в частности)
+@Controller
+public class DefaultController {
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String greeting(Model model) {
+
+        return "index";
+    }
+
+}
